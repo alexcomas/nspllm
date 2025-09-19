9f1ee402add709502c98e552a086ebfcfa635421
jereml99 <ledwojeremi@gmail.com>
2025-09-19 14:59:13 +0200

    Remove GPT-4 request functions and related logic to streamline API interactions

--- reverie/backend_server/persona/prompt_template/gpt_structure.py
+++ reverie/backend_server/persona/prompt_template/gpt_structure.py
@@ -57,29 +57,6 @@ def ChatGPT_single_request(prompt):
 # ============================================================================
 
 
-def GPT4_request(prompt):
-    """
-    Given a prompt and a dictionary of GPT parameters, make a request to OpenAI
-    server and returns the response.
-    ARGS:
-      prompt: a str prompt
-      gpt_parameter: a python dictionary with the keys indicating the names of
-                     the parameter and the values indicating the parameter
-                     values.
-    RETURNS:
-      a str of GPT-3's response.
-    """
-    temp_sleep()
-
-    try:
-        completion = client.chat.completions.create(model="gpt-4",
-        messages=[{"role": "user", "content": prompt}])
-        return completion.choices[0].message.content
-
-    except:
-        print("ChatGPT ERROR")
-        return "ChatGPT ERROR"
-
 
 def ChatGPT_request(prompt):
     """
@@ -104,47 +81,6 @@ def ChatGPT_request(prompt):
         print("ChatGPT ERROR")
         return "ChatGPT ERROR"
 
-
-def GPT4_safe_generate_response(
-    prompt,
-    example_output,
-    special_instruction,
-    repeat=3,
-    fail_safe_response="error",
-    func_validate=None,
-    func_clean_up=None,
-    verbose=False,
-):
-    prompt = 'GPT-3 Prompt:\n"""\n' + prompt + '\n"""\n'
-    prompt += (
-        f"Output the response to the prompt above in json. {special_instruction}\n"
-    )
-    prompt += "Example output json:\n"
-    prompt += '{"output": "' + str(example_output) + '"}'
-
-    if verbose:
-        print("CHAT GPT PROMPT")
-        print(prompt)
-
-    for i in range(repeat):
-        try:
-            curr_gpt_response = GPT4_request(prompt).strip()
-            end_index = curr_gpt_response.rfind("}") + 1
-            curr_gpt_response = curr_gpt_response[:end_index]
-            curr_gpt_response = json.loads(curr_gpt_response)["output"]
-
-            if func_validate(curr_gpt_response, prompt=prompt):
-                return func_clean_up(curr_gpt_response, prompt=prompt)
-
-            if verbose:
-                print("---- repeat count: \n", i, curr_gpt_response)
-                print(curr_gpt_response)
-                print("~~~~")
-
-        except:
-            pass
-
-    return False
 
 
 def ChatGPT_safe_generate_response(
@@ -328,47 +264,9 @@ def safe_generate_response(
     return fail_safe_response
 
 
-def get_embedding(text, model="text-embedding-ada-002"):
+def get_embedding(text, model="text-embedding-3-small"):
     text = text.replace("\n", " ")
     if not text:
         text = "this is blank"
     return client.embeddings.create(input=[text], model=model).data[0].embedding
 
-
-if __name__ == "__main__":
-    gpt_parameter = {
-        "engine": "text-davinci-003",
-        "max_tokens": 50,
-        "temperature": 0,
-        "top_p": 1,
-        "stream": False,
-        "frequency_penalty": 0,
-        "presence_penalty": 0,
-        "stop": ['"'],
-    }
-    curr_input = ["driving to a friend's house"]
-    prompt_lib_file = "prompt_template/test_prompt_July5.txt"
-    prompt = generate_prompt(curr_input, prompt_lib_file)
-
-    def __func_validate(gpt_response):
-        if len(gpt_response.strip()) <= 1:
-            return False
-        if len(gpt_response.strip().split(" ")) > 1:
-            return False
-        return True
-
-    def __func_clean_up(gpt_response):
-        cleaned_response = gpt_response.strip()
-        return cleaned_response
-
-    output = safe_generate_response(
-        prompt,
-        gpt_parameter,
-        5,
-        "rest",
-        __func_validate,
-        __func_clean_up,
-        True,
-    )
-
-    print(output)
